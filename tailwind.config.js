c64ec03dadf9f31b5b6dbd5f6f63f2a8a329485c
Gregory Daynes <gregdaynes@gmail.com>
2022-11-20 11:36:35 -0800

    feat: netlifycms and eleventy build

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
     "./**/*.{html,css}"
   ],
   theme: {
+    container: {
+      center: true,
+    },
     extend: {
       fontFamily: {
         'sans': ['Montserrat', ...defaultTheme.fontFamily.sans],
@@ -29,7 +32,25 @@ module.exports = {
       }
     },
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#312E81",
+          "secondary": "#D926A9",
+          "accent": "#1FB2A6",
+          "neutral": "#191D24",
+          "base-100": "#2A303C",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   plugins: [
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
+    require('daisyui')
   ],
 }
