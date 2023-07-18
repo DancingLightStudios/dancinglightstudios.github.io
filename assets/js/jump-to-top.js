e30ef019d4b90db725feab1489fd6bb26b15c39f
gregdaynes <gregdaynes@users.noreply.github.com>
2023-07-18 18:18:48 +0000

    deploy: d89ed798ca47bed2a98345e4e39ca75994237b96

--- assets/js/jump-to-top.js
+++
@@ -1,9 +0,0 @@
-/* global window */
-window.addEventListener('scroll', () => {
-  const backToTopLink = document.querySelector('.jump-to-top-link');
-  const position = window.scrollY;
-
-  position >= 800
-    ? backToTopLink.classList.add('visible')
-    : backToTopLink.classList.remove('visible');
-});
